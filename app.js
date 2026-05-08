2fb6e0acc73417d17b8fcb295f2caa7875397a2f
Hardi-Kun <m.hardiansyah0406@gmail.com>
2026-05-08 20:54:10 +0700

    tambah c_alat dan update app.js

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express       = require('express')
 const app           = express()
 const port          = 5000
+const c_alat        = require('./controller/c_alat')
 
 app.use( express.urlencoded({extended:false}))
 app.set('view engine', 'ejs') 
 app.set('views', './view') 
 
-app.get('/' , )
+// ======= ROUTE ALAT =======
+app.get('/alat',                        c_alat.hal_index)
+app.get('/alat/tambah',                 c_alat.hal_form_tambah)
+app.post('/alat/tambah',                c_alat.proses_tambah)
+app.get('/alat/edit/:kode',             c_alat.hal_form_edit)
+app.post('/alat/edit/:kode',            c_alat.proses_edit)
+app.get('/alat/hapus/:kode',            c_alat.proses_hapus)
+app.get('/alat/filter/:kategori',       c_alat.filter_kategori)
+app.get('/alat/status/:status',         c_alat.filter_status)
 
-
+app.get('/', (req, res) => {
+    res.redirect('/alat')
+})
 
 
 app.listen(port, ()=>{
---
+++ controller/c_alat.js
@@ -0,0 +1,89 @@
+const moment        = require('moment')
+const alatModel     = require('../model/alatModel')
+
+module.exports =
+{
+    hal_index: async (req, res) => {
+        let data = {
+            req         : req,
+            alat        : await alatModel.getAll(),
+            rekap       : await alatModel.getRekap(),
+            notifikasi  : req.query.notif,
+            moment      : moment,
+        }
+        res.render('v_inventaris/alat/index', data)
+    },
+
+    hal_form_tambah: async (req, res) => {
+        let data = {
+            req         : req,
+            notifikasi  : req.query.notif,
+        }
+        res.render('v_inventaris/alat/form-tambah', data)
+    },
+
+    proses_tambah: async (req, res) => {
+        try {
+            let insert = await alatModel.insert(req)
+            if (insert.affectedRows > 0) {
+                res.redirect('/alat?notif=Alat berhasil ditambahkan')
+            }
+        } catch (error) {
+            res.redirect('/alat?notif=' + error.message)
+        }
+    },
+
+    hal_form_edit: async (req, res) => {
+        let kode = req.params.kode
+        let data = {
+            req         : req,
+            alat        : await alatModel.getByKode(kode),
+            notifikasi  : req.query.notif,
+        }
+        res.render('v_inventaris/alat/form-edit', data)
+    },
+
+    proses_edit: async (req, res) => {
+        try {
+            let update = await alatModel.update(req)
+            if (update.affectedRows > 0) {
+                res.redirect('/alat?notif=Alat berhasil diupdate')
+            }
+        } catch (error) {
+            res.redirect('/alat?notif=' + error.message)
+        }
+    },
+
+    proses_hapus: async (req, res) => {
+        try {
+            let hapus = await alatModel.hapus(req.params.kode)
+            if (hapus.affectedRows > 0) {
+                res.redirect('/alat?notif=Alat berhasil dihapus')
+            }
+        } catch (error) {
+            res.redirect('/alat?notif=' + error.message)
+        }
+    },
+
+    filter_kategori: async (req, res) => {
+        let data = {
+            req         : req,
+            alat        : await alatModel.getByKategori(req.params.kategori),
+            rekap       : await alatModel.getRekap(),
+            notifikasi  : req.query.notif,
+            moment      : moment,
+        }
+        res.render('v_inventaris/alat/index', data)
+    },
+
+    filter_status: async (req, res) => {
+        let data = {
+            req         : req,
+            alat        : await alatModel.getByStatus(req.params.status),
+            rekap       : await alatModel.getRekap(),
+            notifikasi  : req.query.notif,
+            moment      : moment,
+        }
+        res.render('v_inventaris/alat/index', data)
+    },
+}
